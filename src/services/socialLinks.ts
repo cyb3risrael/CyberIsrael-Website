5c39c14babdb25ef5c80040e023147a4b8c20fed
Amit Aviv <amitaviv01@gmail.com>
2026-08-18 11:37:24 +0300

    fix: update discord link to the correct URL

--- src/services/socialLinks.ts
+++ src/services/socialLinks.ts
@@ -1,5 +1,5 @@
 export const socialLinks = {
-  discord:   'https://discord.gg/Xz8gsvpBp',
+  discord:   'https://discord.gg/3zdFPBFkb3',
   whatsapp:  'https://chat.whatsapp.com/EdGa1iawSFj2b6RMFdp4Jk',
   drive:     'https://drive.google.com/drive/folders/1FpbtSTb0ztKClrIKZT1HdlGbLENTj_hj?usp=sharing',
   instagram: 'https://www.instagram.com/cyb3r.israel?igsh=Mjh0aGZqOHhoOXFi',
