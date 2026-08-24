ac9577f54d694572a0dc73a89eb0d0d19fd0fa7a
DrYonatan <talagin11@gmail.com>
2026-08-24 13:52:28 +0300

    add empty resources page and link from navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React, { Suspense, lazy } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from '@/context/ThemeContext'
-import { LangProvider } from '@/context/LangContext'
-import RootLayout from '@/components/layout/RootLayout'
-import ComingSoonPage from './pages/ComingSoonPage'
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/context/ThemeContext";
+import { LangProvider } from "@/context/LangContext";
+import RootLayout from "@/components/layout/RootLayout";
+import ComingSoonPage from "./pages/ComingSoonPage";
 
 // Lazy load pages for code splitting
-const HomePage = lazy(() => import('@/pages/HomePage'))
-const ArticlesPage = lazy(() => import('@/pages/ArticlesPage'))
-const ArticlePage = lazy(() => import('@/pages/ArticlePage'))
-const ImpactPage = lazy(() => import('@/pages/ImpactPage'))
-const CollaboratePage = lazy(() => import('@/pages/CollaboratePage'))
-const NotFoundPage = lazy(() => import('@/pages/NotFoundPage'))
+const HomePage = lazy(() => import("@/pages/HomePage"));
+const ArticlesPage = lazy(() => import("@/pages/ArticlesPage"));
+const ArticlePage = lazy(() => import("@/pages/ArticlePage"));
+const ImpactPage = lazy(() => import("@/pages/ImpactPage"));
+const CollaboratePage = lazy(() => import("@/pages/CollaboratePage"));
+const ResourcesPage = lazy(() => import("@/pages/ResourcesPage"));
+const NotFoundPage = lazy(() => import("@/pages/NotFoundPage"));
 
 const PageLoader: React.FC = () => (
   <div className="min-h-screen flex items-center justify-center">
     <div className="flex flex-col items-center gap-4">
       <div className="w-10 h-10 border-2 border-cyber-green/30 border-t-cyber-green rounded-full animate-spin" />
-      <span className="font-display text-xs tracking-widest text-cyber-green/60">LOADING...</span>
+      <span className="font-display text-xs tracking-widest text-cyber-green/60">
+        LOADING...
+      </span>
     </div>
   </div>
-)
+);
 
 const App: React.FC = () => {
   return (
@@ -37,6 +40,7 @@ const App: React.FC = () => {
                 <Route path="/coming-soon/:name" element={<ComingSoonPage />} />
                 <Route path="impact" element={<ImpactPage />} />
                 <Route path="collaborate" element={<CollaboratePage />} />
+                <Route path="resources" element={<ResourcesPage />} />
                 <Route path="*" element={<NotFoundPage />} />
               </Route>
             </Routes>
@@ -44,7 +48,7 @@ const App: React.FC = () => {
         </BrowserRouter>
       </LangProvider>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/ResourcesPage.tsx
@@ -0,0 +1,5 @@
+const ResourcesPage: React.FC = () => {
+  return <div></div>;
+};
+
+export default ResourcesPage;
